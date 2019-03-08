de5989c30647ef63c262644418c3a84f4fb232d2
Jordan <jwtrumbull@yahoo.com>
2019-03-07 16:13:01 -0800

    shamefulbot things added

--- react.js
+++ react.js
@@ -1,2 +1,28 @@
+// TARGET SUM - Starting Template
 
-ReactDOM.render(<h1>hello world</h1>, document.getElementById("root"));
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="help">
+          Pick 4 numbers that sum to the target in 15 seconds
+        </div>
+        <div className="target">42</div>
+        <div className="challenge-numbers">
+          <div className="number">8</div>
+          <div className="number">5</div>
+          <div className="number">12</div>
+          <div className="number">13</div>
+          <div className="number">5</div>
+          <div className="number">16</div>
+        </div>
+        <div className="footer">
+          <div className="timer-value">15</div>
+          <button>Start</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Game />, mountNode);
